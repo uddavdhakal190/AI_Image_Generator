e50603c5502abda844b7b2780ae95781929271ba
uddavdhakal190 <uddavdhakal190@gmail.com>
2025-09-06 21:49:54 +0300

    Add utility functions and update component exports - Add utility functions for API calls and image handling - Update component index exports - Update project documentation

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,8 @@ import Card from './Card';
 import FormField from './FormField';
 import Loader from './Loader';
 
-export { Card, FormField, Loader };
+export { 
+        Card,
+        FormField,
+        Loader 
+    };
